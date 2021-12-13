a501454ffec25edbd908bee11538663ec9a19b12
aleezel <89430034+aleezel@users.noreply.github.com>
2021-12-13 01:13:03 -0600

    Transiciones y demás pantallas
    Pantallas de menú, juego random, juego con amigos y se añadió transiciones en muchas escenas.
    Se creó y añadió la escena de "MenuOptions"

---
+++ Memwar/Assets/Scripts/UI/NavbarButtons.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavbarButtons : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("MenuOptions");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
